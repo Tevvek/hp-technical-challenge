102748dad17dd284b0f57f30a40abcc32dcfae25
Tevvek <daniil@poyda-systems.com>
2024-06-16 21:30:59 +0200

    Fill album component with data. Style it.
    Used a 100x100 image because that is the thumbnail we are getting from iTunes.

--- task_1/frontend/src/components/Album.vue
+++ task_1/frontend/src/components/Album.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { type Album } from '@/../../types';
+const props = defineProps<Album>();
 </script>
 
 <template>
-    <div class="size-40 bg-white"></div>
+    <a class="hover:scale-110 transition duration-300" :href="props.collectionViewUrl" target="_blank" rel="noopener noreferrer" :title="props.collectionName" v-if="props.wrapperType === 'collection'">
+        <img :src="props.artworkUrl100" :alt="`Album cover for ${props.collectionName}`" class="size-[100px] rounded-lg" />
+        <p class="text-sm font-semibold line-clamp-2 mt-2">{{ props.collectionName }}</p>
+        <p class="text-sm line-clamp-2" :title="props.artistName">{{ props.artistName }}</p>
+    </a>
 </template>
