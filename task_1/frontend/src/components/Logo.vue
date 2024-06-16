ed4ef28cd6ba19bbe14c1fca6f4834e87d0ce036
Daniil Teterevkov <daniil.teterevkov@ingrammicro.com>
2024-06-16 20:41:38 +0200

    Create Logo component using HP image

---
+++ task_1/frontend/src/components/Logo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import logo from '@/assets/hp.png'
+</script>
+
+<template>
+    <div class="bg-white p-1 rounded-full size-16">
+        <img :src="logo" alt="HP Logo" />
+    </div>
+</template>
