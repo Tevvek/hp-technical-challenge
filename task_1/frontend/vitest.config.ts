bf63c16693fedf4e5d8e22c91d8c6eed2e2cf0fc
Daniil Teterevkov <daniil.teterevkov@ingrammicro.com>
2024-06-19 01:39:00 +0200

    Add packages and setup testing environment for frontend

---
+++ task_1/frontend/vitest.config.ts
@@ -0,0 +1,17 @@
+// vitest.config.ts
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
