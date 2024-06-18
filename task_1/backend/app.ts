743d224ffb3b2e4f8e850fa6cd05a9e40ff73918
Tevvek <daniil@poyda-systems.com>
2024-06-19 00:25:01 +0200

    Split backend app and server so that endpoints can be easily tested

---
+++ task_1/backend/app.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { AlbumResponse } from "./types";
+
+const app = express();
+
+const ITUNES_URL = "https://itunes.apple.com";
+const ARTIST_ID = "408932";
+const ITUNES_URL_ARTIST_ALBUMS = `${ITUNES_URL}/lookup?id=${ARTIST_ID}&entity=album`;
+
+app.get("/albums", async (req, res) => {
+  try {
+    // Fetch the albums from the iTunes API
+    const result = await fetch(ITUNES_URL_ARTIST_ALBUMS);
+
+    // Check if the request was successful
+    if (!result.ok) {
+      return res.status(result.status).send("Could not fetch albums");
+    }
+
+    // Parse the response
+    let albumsResponse: AlbumResponse;
+    try {
+      albumsResponse = (await result.json()) as AlbumResponse;
+    } catch (jsonError) {
+      console.error(jsonError);
+      return res.status(500).send("Could not parse JSON");
+    }
+    const albums = albumsResponse.results;
+
+    // Filter out the unique albums
+    const seen = new Set();
+    const uniqueAlbums = albums.filter((album) => {
+      const duplicate = seen.has(album.collectionName);
+      seen.add(album.collectionName);
+      return !duplicate;
+    });
+
+    // Return the unique albums
+    return res.status(200).json(uniqueAlbums);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+export default app;
--- task_1/backend/server.ts
+++ task_1/backend/server.ts
@@ -1,13 +1,10 @@
-import express from "express";
 import cors from "cors";
-import { AlbumResponse } from "./types";
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config({
   path: "../.env",
 });
-
-const app = express();
 
 const CORS_OPTIONS = {
   origin: `http://localhost:${process.env.FRONTEND_PORT}`,
@@ -15,46 +12,6 @@ const CORS_OPTIONS = {
 app.use(cors(CORS_OPTIONS));
 
 const port = process.env.BACKEND_PORT || 4000;
-
-const ITUNES_URL = "https://itunes.apple.com";
-const ARTIST_ID = "408932";
-const ITUNES_URL_ARTIST_ALBUMS = `${ITUNES_URL}/lookup?id=${ARTIST_ID}&entity=album`;
-
-app.get("/albums", async (req, res) => {
-  try {
-    // Fetch the albums from the iTunes API
-    const result = await fetch(ITUNES_URL_ARTIST_ALBUMS);
-
-    // Check if the request was successful
-    if (!result.ok) {
-      return res.status(result.status).send("Could not fetch albums");
-    }
-
-    // Parse the response
-    let albumsResponse: AlbumResponse;
-    try {
-      albumsResponse = (await result.json()) as AlbumResponse;
-    } catch (jsonError) {
-      console.error(jsonError);
-      return res.status(500).send("Could not parse JSON");
-    }
-    const albums = albumsResponse.results;
-
-    // Filter out the unique albums
-    const seen = new Set();
-    const uniqueAlbums = albums.filter((album) => {
-      const duplicate = seen.has(album.collectionName);
-      seen.add(album.collectionName);
-      return !duplicate;
-    });
-
-    // Return the unique albums
-    return res.status(200).json(uniqueAlbums);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
