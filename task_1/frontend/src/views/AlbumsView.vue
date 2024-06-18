2055ec477c87bc1adbf60864f82168f82ed4d8c0
Tevvek <daniil@poyda-systems.com>
2024-06-18 21:11:26 +0200

    Remove use of provider and call fetch of store from view. This is needed only once.

--- task_1/frontend/src/views/AlbumsView.vue
+++ task_1/frontend/src/views/AlbumsView.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import AlbumsProvider from '@/providers/AlbumsProvider.vue';
-import Footer from '@/components/Footer.vue';
+import Header from '@/sections/Header.vue';
+import Main from '@/sections/Main.vue';
+import Footer from '@/sections/Footer.vue';
+import { useAlbumsStore } from '@/store/albumsStore';
+
+const albumsStore = useAlbumsStore();
+albumsStore.fetchAlbums();
 </script>
 
 <template>
     <div class="bg-gray-50 min-h-screen grid grid-rows-[min-content,1fr,50px]">
+        <Header />
 
-        <Suspense>
-            <AlbumsProvider />
-        </Suspense>
+        <Main />
 
-        <!-- Little footer for more info -->
         <Footer />
     </div>
 </template>
