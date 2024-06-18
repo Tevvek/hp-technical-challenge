209ff07afc552bb36b676b7050f72b7e8b807b2d
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:20:10 +0200

    Create the only AlbumsView

---
+++ task_1/frontend/src/views/AlbumsView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AlbumsProvider from '@/providers/AlbumsProvider.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+    <div class="bg-gray-50 min-h-screen grid grid-rows-[min-content,1fr,50px]">
+
+        <Suspense>
+            <AlbumsProvider />
+        </Suspense>
+
+        <!-- Little footer for more info -->
+        <Footer />
+    </div>
+</template>
