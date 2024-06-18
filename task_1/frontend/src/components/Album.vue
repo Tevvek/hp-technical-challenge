2af9383ecb3a0ee5ce6e440ff8de3634e8716469
Tevvek <daniil@poyda-systems.com>
2024-06-18 21:11:46 +0200

    Figured out thumbnails could be bigger instead of 100x100 for better UI

--- task_1/frontend/src/components/Album.vue
+++ task_1/frontend/src/components/Album.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { type Album } from '@/../../types';
+import { Album } from '@/../../types';
 const props = defineProps<Album>();
+
+// Big thumbnail, inferred by the use of the URL
+const thumbnailURL = props.artworkUrl100.replace('100x100bb.jpg', '250x250bb.jpg');
 </script>
 
 <template>
-    <a class="hover:scale-110 transition duration-300" :href="props.collectionViewUrl" target="_blank" rel="noopener noreferrer" :title="props.collectionName" v-if="props.wrapperType === 'collection'">
-        <img :src="props.artworkUrl100" :alt="`Album cover for ${props.collectionName}`" class="size-[100px] rounded-lg" />
+    <a class="hover:scale-110 transition duration-300 w-[250px]" :href="props.collectionViewUrl" target="_blank"
+        rel="noopener noreferrer" :title="props.collectionName" v-if="props.wrapperType === 'collection'">
+        <img :src="thumbnailURL" :alt="`Album cover for ${props.collectionName}`" class="rounded-lg" />
         <p class="text-sm font-semibold line-clamp-2 mt-2">{{ props.collectionName }}</p>
         <p class="text-sm line-clamp-2" :title="props.artistName">{{ props.artistName }}</p>
     </a>
