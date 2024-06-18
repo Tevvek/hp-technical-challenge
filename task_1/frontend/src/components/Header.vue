a26125792094c92efa648fea0e792e5d07d3ef43
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:21:05 +0200

    Create Header component that uses the SearchBox

---
+++ task_1/frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Logo from '@/components/Logo.vue'
+import SearchBox from '@/components/SearchBox.vue'
+</script>
+
+<template>
+    <header
+        class="bg-primary grid place-items-center md:grid-cols-[100px,1fr,100px] md:place-content-center px-10 py-4">
+
+        <!-- Logo wrapper -->
+        <div class="flex items-center">
+            <Logo />
+        </div>
+
+        <!-- Centered search box wrapper -->
+        <div class="flex justify-center">
+
+            <!-- Search box group -->
+            <SearchBox />
+        </div>
+    </header>
+</template>
