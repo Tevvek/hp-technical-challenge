a2c6d6c50979080c69ee77c8a64502e95e4963cb
Tevvek <daniil@poyda-systems.com>
2024-06-18 14:03:29 +0200

    Make Logo anchor redirect to main page of our localhost instead of HP website
    For debugging reasons

--- task_1/frontend/src/components/Logo.vue
+++ task_1/frontend/src/components/Logo.vue
@@ -3,7 +3,7 @@ import logo from '@/assets/hp.png'
 </script>
 
 <template>
-    <a href="https://www.hp.com/es-es/home.html" class="bg-white p-1 rounded-full size-16 hover:scale-110 transition duration-300" target="_blank" rel="noopener noreferrer">
+    <a href="/" class="bg-white p-1 rounded-full size-16 hover:scale-110 transition duration-300">
         <img :src="logo" alt="HP Logo" />
     </a>
 </template>
