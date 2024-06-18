fbad36e167787fb312c7cafae9f2011fb2fbd1b8
Tevvek <daniil@poyda-systems.com>
2024-06-18 14:01:50 +0200

    Create SearchBox component that debounces the value written and sets the search value in query params

---
+++ task_1/frontend/src/components/SearchBox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col gap-y-1">
+        <label for="search" class="text-sm text-white">Search</label>
+        <input type="search" id="search" name="search" class="text-sm rounded-lg px-2 py-1" v-model="search" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useDebounce } from '@/composables/useDebounce';
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute()
+const router = useRouter()
+
+const search = ref(route.query.search ?? '');
+const debouncedSearch = useDebounce(search, 500)
+
+watch(debouncedSearch, () => {
+    router.replace({ query: { search: debouncedSearch.value } })
+})
+
+</script>
