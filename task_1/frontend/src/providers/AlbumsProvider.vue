09aeb9357574623c98f768242955a0cb56673fbe
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:20:46 +0200

    Create albums provider that fetches once the albums and then provides them to its children

---
+++ task_1/frontend/src/providers/AlbumsProvider.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+import Main from '@/views/main.vue';
+import { type Album } from '@/../../types';
+import { ref, provide } from 'vue';
+
+const response = await fetch(`/api/albums`)
+const albums = await response.json()
+
+const albumsRef = ref<Album[]>(albums);
+provide('albums', albumsRef);
+
+</script>
+<template>
+    <!-- Top header with logo and search box -->
+    <Header />
+
+    <Suspense>
+        <Main />
+    </Suspense>
+
+</template>
