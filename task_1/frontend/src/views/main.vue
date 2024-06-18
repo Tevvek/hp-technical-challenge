f2799f03d59a59fa4476669a412d91c18d56c952
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:21:41 +0200

    Use the Main component section to filter albums by query params

--- task_1/frontend/src/views/main.vue
+++ task_1/frontend/src/views/main.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
-import Logo from '@/components/Logo.vue'
-import Album from '@/components/Album.vue'
+import Album from '@/components/Album.vue';
+import { computed, inject, Ref } from 'vue';
+import { type Album as AlbumType } from '@/../../types';
+import { useRoute } from 'vue-router';
 
-const response = await fetch('http://localhost:3000/albums')
-const albums = await response.json()
+const albums = inject('albums') as Ref<AlbumType[]>;
+const route = useRoute();
 
-console.log(albums[1])
+const albumsByQueryParams = computed(() => {
+  const search = route.query.search as string;
+
+  if (!search) {
+    return albums.value;
+  }
+
+  return albums.value.filter(album =>
+    album.collectionName?.toLowerCase().includes(search.toLowerCase())
+  );
+});
+
+
 </script>
 
 <template>
-      <!-- Main with albums -->
-      <main class="bg-gray-200 p-10">
+  <!-- Main with albums -->
+  <main class="bg-gray-200 p-10">
 
-        <!-- Albums grid -->
-        <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-10">
-          
-          <!-- One album -->
-          <Album v-for="(album, index) in albums" :key="index" v-bind="album" />
-        </div>
-      </main>
-  </template>
-  
+    <!-- Albums grid -->
+    <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-5">
+
+      <!-- One album -->
+      <Album v-for="(album, index) in albumsByQueryParams" :key="index" v-bind="album" />
+    </div>
+  </main>
+</template>
