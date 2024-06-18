670a1bec0f8a0ef578fb577594364f4b1282f3cc
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:20:22 +0200

    Create small footer component

---
+++ task_1/frontend/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="flex px-10 justify-end items-center bg-primary text-white">
+        <p class="text-xs">
+            HP Technical Challenge -
+            <a href="https://www.linkedin.com/in/daniilteterevkov/" class="font-semibold">Daniil Teterevkov</a>
+        </p>
+    </footer>
+</template>
