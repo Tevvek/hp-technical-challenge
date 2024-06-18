0f493cd1d3f9d3bb806f66c7dbe6645b371a8b70
Tevvek <daniil@poyda-systems.com>
2024-06-18 21:09:32 +0200

    Small improvement on logo

--- task_1/frontend/src/components/Logo.vue
+++ task_1/frontend/src/components/Logo.vue
@@ -3,7 +3,7 @@ import logo from '@/assets/hp.png'
 </script>
 
 <template>
-    <a href="/" class="bg-white p-1 rounded-full size-16 hover:scale-110 transition duration-300">
+    <a href="/" class="bg-white p-1 rounded-full size-12 hover:scale-110 transition duration-300">
         <img :src="logo" alt="HP Logo" />
     </a>
 </template>
