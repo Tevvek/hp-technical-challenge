22c7518d912345c0f6e36a512d09f28ee67fa034
Tevvek <daniil@poyda-systems.com>
2024-06-18 15:19:24 +0200

    Add tailwind forms plugin to remove css defaults

--- task_1/frontend/tailwind.config.js
+++ task_1/frontend/tailwind.config.js
@@ -1,8 +1,14 @@
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#005195",
+      },
+    },
   },
-  plugins: [],
+  plugins: [forms],
 };
